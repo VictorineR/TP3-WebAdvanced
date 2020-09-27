fa0ee38b1910f126f058954c957bf3aaad8991ad
Nexliath <victor.garnier@efrei.net>
2020-09-27 20:49:46 +0200

    Esquisse ex 2 petit 1 et 2

--- efrei-3a-web-ex1-master/client/vue-application.js
+++ efrei-3a-web-ex1-master/client/vue-application.js
@@ -9,8 +9,57 @@ Vue.component('hello-world', {
   template: `<p>Hello{{ message }}!</p>`
 })
 
+Vue.component('number-days', {
+  data: function () {
+    let date = new Date()
+    let month = date.getMonth()
+    let year = date.getFullYear()
+
+    numberDays = new Date(year, month, 0).getDate();
+
+    return {
+      numberD: numberDays
+    }
+  },
+
+  template: `<p>Number of Day {{numberD}}!</p>`
+})
+
+Vue.component('days-month', {
+  data: function () {
+    let date = new Date()
+    let month = date.getMonth()
+    let year = date.getFullYear()
+    numberDays = new Date(year, month, 0).getDate();
+    let i = 0;
+
+    let table = new Array(numberDays);
+    for (i; i < numberDays; i++) {
+      table[i] = i + 1
+    }
+
+    return {
+      numberD: numberDays,
+      monthTable: table
+    }
+
+  },
+
+  template: `<ul>
+            <li v-for="day in monthTable">
+                <p>{{ monthTable}}</p>
+              
+            </li>
+            </ul>`
+})
+
 var app = new Vue({
   el: '#app',
-  data: { },
+  data: {
+  },
+
+  methods: {
+  },
+
   components: { HelloOtherWorld }
 })
