01cbc42b1c11142dcd6c88b14952288b60822478
Nexliath <victor.garnier@efrei.net>
2020-09-28 19:49:41 +0200

    Ex2 et Ex3 finis

--- efrei-3a-web-ex1-master/client/vue-application.js
+++ efrei-3a-web-ex1-master/client/vue-application.js
@@ -1,65 +1,99 @@
 const HelloOtherWorld = window.httpVueLoader('./components/HelloOtherWorld.vue')
 
-Vue.component('hello-world', {
-  data: function () {
-    return {
-      message: ', World'
-    }
-  },
-  template: `<p>Hello{{ message }}!</p>`
-})
+Vue.component('days-of-month',{
 
-Vue.component('number-days', {
-  data: function () {
-    let date = new Date()
-    let month = date.getMonth()
-    let year = date.getFullYear()
-
-    numberDays = new Date(year, month, 0).getDate();
-
-    return {
-      numberD: numberDays
+  data: function() {
+    return{
+      principalMonths: ["January", "February", "Mars", "April", "May", "June", "Jully", "August", "September", "October", "November", "December"],
     }
   },
 
-  template: `<p>Number of Day {{numberD}}!</p>`
-})
+  props: ['selectedMonth','selectedYear','Holiday'],
 
-Vue.component('days-month', {
-  data: function () {
-    let date = new Date()
-    let month = date.getMonth()
-    let year = date.getFullYear()
-    numberDays = new Date(year, month, 0).getDate();
-    let i = 0;
+  computed:{
+    days: function(){
+      var numberDays = new Date(this.selectedYear, this.selectedMonth, 0).getDate();
+      var firstDay = new Date(this.selectedYear, this.selectedMonth - 1, 0).getDay();
+      var principalDays=["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+      var daysTable = [];
 
-    let table = new Array(numberDays);
-    for (i; i < numberDays; i++) {
-      table[i] = i + 1
-    }
+      for (var i = 1; i <= numberDays; i++){
+        daysTable.push(i.toString() +" : " + principalDays[firstDay]);
+        firstDay = (firstDay + 1) % 7;
+      }
 
-    return {
-      numberD: numberDays,
-      monthTable: table
-    }
+      return daysTable; 
+    },
 
+    months: function(){
+      monthsTable = [];
+      for (var i=1; i < 13; i++){
+        months.push(i);
+      }
+
+      return monthsTable;
+    },
+
+    years: function(){
+      years = []
+      for (var i=1990; i <2030; i++){
+        years.push(i);
+      }
+
+      return years;
+    },
+
+    currentMonth: function(){
+
+      return this.principalMonths[this.selectedMonth - 1];
+    },
   },
 
-  template: `<ul>
-            <li v-for="day in monthTable">
-                <p>{{ monthTable}}</p>
-              
-            </li>
-            </ul>`
+  template: `
+    <div>
+      <h3> {{currentMonth}} </h3>
+      <ul>
+          <li v-for="day in days">
+              {{day}}
+          </li>
+      </ul>
+    </div>
+  `
+});
+
+Vue.component('days-of-year',{
+  props: ['year'],
+  computed:{
+    Holidays: function(){
+      fetch('https://date.nager.at/api/v2/PublicHolidays/2020/FR', {mode: 'no-cors'})
+      .then(response => response.json())
+      .then(function (data) {
+          console.log('data', data);
+          
+          return data;
+      })
+    }
+  },
+
+  template: `
+    <div class="calendar">
+      <h2>{{year}}</h2>
+      <days-of-month v-for="index in 12" v-bind:key="index" :selectedMonth="index" :selectedYear="year" :Holiday="Holidays" class="month"></days-of-month>
+    </div>
+  `
 })
+
 
 var app = new Vue({
   el: '#app',
-  data: {
+  data:{
+    years: [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020],
   },
 
-  methods: {
-  },
+  computed:{
+    currentDays: function() {
 
-  components: { HelloOtherWorld }
+      return new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0).getDate();
+    },
+  }
 })
